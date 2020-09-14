b7629ad7faefdc457db030074ac12a44a4f5b897
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-14 14:55:09 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,6 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
@@ -12,6 +12,7 @@ def second_challenge
       :heroine => {},
       :heroine_friends => []
    }
+}
 
   
 end
